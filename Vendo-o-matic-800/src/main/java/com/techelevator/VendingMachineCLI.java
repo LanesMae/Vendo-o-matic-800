7c3bd39bcfad72122cb586761105cfa589740aca
LanesMae <103223392+LanesMae@users.noreply.github.com>
2022-07-18 19:31:15 -0400

    Add files via upload

---
+++ Vendo-o-matic-800/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+
+import com.techelevator.view.Menu;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class VendingMachineCLI {
+
+	public static void main(String[] args) {
+
+
+	}
+}
